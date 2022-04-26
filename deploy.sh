2dcd4dffe6c00aba02f5e850b1bf565a84eef05a
Zerounary <850657663@qq.com>
2022-04-26 13:07:10 +0800

    run after push

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
-
-app="demo-0.0.1-SNAPSHOT"
+cd /home/app
+app="demo-0.0.1-SNAPSHOT.jar"
+kill -9 $(lsof -t -i:9000)
 # kill server pid if exists
-if test -n "$(pidof echo-demo)"; then
-  echo "Killing server"
-  kill -9 $(pidof echo-demo)
+if [ -f $app.pid ]; then
+    kill -9 $(cat $app.pid)
+    rm -f $app.pid
 fi
+
 # 避免nohup命令不终止
 echo "restart server"
-nohup java -jar /home/app/demo-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+nohup ./$app >/dev/null 2>&1 &
+echo $! > $app.pid
 echo "restart server done"
